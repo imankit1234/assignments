5c373330f4ba81f9b5d82e7c0c3ee18614b22b99
Ankit kumar <nks823001@gmail.com>
2023-12-07 00:10:34 +0530

    Update calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -1,71 +1,41 @@
 class Calculator {
-  constructor() {
-    this.result = 0;
+  constructor(){
+    this.result=0;
   }
-
-  add(num) {
-    this.result += num;
+  add(value){
+    this.result+=value;
   }
-
-  subtract(num) {
-    this.result -= num;
+  subtract(value){
+    this.result-=value;
   }
-
-  multiply(num) {
-    this.result *= num;
+  multiply(value){
+    this.result*=value;
   }
-
-  divide(num) {
-    if (num === 0) {
-      throw new Error("Cannot divide by zero");
+  divide(value){
+    if (value==0) 
+    throw new Error;
+    else{
+      this.result/=value;
     }
-    this.result /= num;
   }
-
-  clear() {
-    this.result = 0;
+  clear(){
+    this.result=0;
   }
-
-  getResult() {
+  getResult(){
     return this.result;
   }
-
-  calculate(expression) {
-    const cleanedExpression = expression.replace(/\s+/g, '');
-    const numbers = cleanedExpression.match(/(\d+(\.\d+)?)|(-\d+(\.\d+)?)|(\+\d+(\.\d+)?)|(\*\d+(\.\d+)?)|(-\d+(\.\d+)?)|(\(\d+(\.\d+)?\))|\/\d+(\.\d+)?/g);
-
-    if (!numbers) {
-      throw new Error("Invalid expression");
-    }
-
-    const operators = cleanedExpression.replace(/\d+(\.\d+)?/g, '').split('').filter(char => char.trim() !== '');
-
-    numbers.forEach((token, index) => {
-      if (token.startsWith('(')) {
-        this.calculate(token.slice(1, -1));
-      } else if (token.startsWith('+')) {
-        this.add(parseFloat(token.slice(1)));
-      } else if (token.startsWith('-')) {
-        this.subtract(parseFloat(token.slice(1)));
-      } else if (token.startsWith('*')) {
-        this.multiply(parseFloat(token.slice(1)));
-      } else if (token.startsWith('/')) {
-        this.divide(parseFloat(token.slice(1)));
-      } else {
-        this.add(parseFloat(token));
-      }
-
-      if (operators[index] === '-') {
-        // Handle subtraction
-        this.subtract(parseFloat(numbers[index + 1]));
-      } else if (operators[index] === '*') {
-        // Handle multiplication
-        this.multiply(parseFloat(numbers[index + 1]));
-      } else if (operators[index] === '/') {
-        // Handle division
-        this.divide(parseFloat(numbers[index + 1]));
-      }
-    });
+  calculate(expr){
+    expr=expr.replace(/\s/g,"");
+    if(/[a-zA-z@!$#]/g.test(expr)){
+      throw new Error;
+   }
+   else if(expr.includes("/0")){
+     throw new Error;
+   }
+   else{
+      return this.result=eval(expr);
+   }
+    
   }
 }
 
